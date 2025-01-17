3422ad9a25de24440a54b5c06f8e77e15b57729c
samu2421 <dheresamiksha@gmail.com>
2025-01-17 11:56:00 +0100

    changed the title and added an extra line at the end.

--- addition.py
+++ addition.py
@@ -1,4 +1,4 @@
-def add_nos(a, b):
+def addition(a, b):
     """
     This function takes two numbers as input and returns their sum.
     """
@@ -7,5 +7,6 @@ def add_nos(a, b):
 
 num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
-result = add_nos(num_1, num_2)
+result = addition(num_1, num_2)
 print(f"The sum of two nos is: {result}")
+print("The function diplays the addtion of the numbers. ")
