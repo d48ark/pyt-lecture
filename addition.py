f72580144f045ec7db59f2bbf016a9dc8af87366
samu2421 <dheresamiksha@gmail.com>
2025-01-17 11:12:18 +0100

    addition function added.

---
+++ addition.py
@@ -0,0 +1,11 @@
+def add_nos(a, b):
+    """
+    This function takes two numbers as input and returns their sum.
+    """
+    return a + b
+
+
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+result = add_nos(num_1, num_2)
+print(f"The sum of two nos is: {result}")
